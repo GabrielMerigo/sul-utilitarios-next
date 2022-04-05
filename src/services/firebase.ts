0f469fa872629debd0edfc96345d3a30db60304b
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-04-05 17:10:56 -0300

    variables env

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore, collection, getDocs, getDoc, doc } from 'firebase/firesto
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDc9ad2YMJ8g_y7z8ATC-bR4BJzaxEj2pw",
-  authDomain: "sul-utilitarios-formulario.firebaseapp.com",
-  projectId: "sul-utilitarios-formulario",
-  storageBucket: "sul-utilitarios-formulario.appspot.com",
-  messagingSenderId: "1092747169054",
-  appId: "1:1092747169054:web:0601d953e0560f6bc2cd0f",
-  measurementId: "G-7GBPNRGG1E"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 const app = firebase.initializeApp(firebaseConfig);
