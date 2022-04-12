0b14a6592cea1b24fba3142a3ad88da70f159987
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-04-11 22:16:26 -0300

    tornando home responsiva

--- src/styles/AboutContainer.ts
+++ src/styles/AboutContainer.ts
@@ -3,13 +3,19 @@ import styled from "styled-components";
 export const AboutContainer = styled.section`
   display: flex;
   flex-direction:column;
+
   .imgCompany{
     background-color: rgb(200, 200, 500);
     width: 60rem;
     margin: 0 auto;
     padding-top: 25rem;
     border: 1px solid black;
+
+    @media(max-width: 900px) {
+      width: 30rem;
+    }
   }
+
   .aboutCompany{
     margin: 1rem auto;
     max-width: 950px;
@@ -17,17 +23,41 @@ export const AboutContainer = styled.section`
       text-align: left;
       font-size: 1.5rem;
     }
+
+    @media(max-width: 900px) {
+      margin-left: 0.8rem;
+
+      h2{
+        text-align: left;
+        font-size: 1.3rem;
+      }
+    }
   }
+
   .aboutOwner{
     margin: 1rem auto;
     max-width: 950px;
     display: flex;
     justify-content: row;
+
     .imgOwner{
       width:150rem;
       border: 1px solid black;
       height: 22rem;
       margin-right: 1rem;
+    }
+
+    @media(max-width: 900px) {
+      flex-direction: column;
+      
+      .imgOwner{
+        width:29rem;
+        margin: 1rem;
+      }
+
+      p{
+        padding: 1rem;
+      }
     }
   }
 `;
